8622e74fc66508cb2bddb38402eeb837daa8a833
Aritra Kundu <107234008+Bapan95@users.noreply.github.com>
2024-11-23 21:46:22 +0530

    ok

--- resources/views/membership/show.blade.php
+++ resources/views/membership/show.blade.php
@@ -8,12 +8,7 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg p-6">
             <table class="table-auto w-full text-left text-gray-700 dark:text-gray-200">
-                @foreach ([
-                    'branch', 'name', 'date_of_birth', 'age', 'date_of_receipt', 'class', 'occupation',
-                    'front', 'newspaper', 'monthly_income', 'levy_rate', 'january', 'february',
-                    'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october',
-                    'november', 'december', 'state_relief_fund', 'one_day_income', 'aid_fago', 'comment'
-                ] as $field)
+                @foreach (['branch', 'name', 'date_of_birth', 'age', 'date_of_receipt', 'class', 'occupation', 'front', 'newspaper', 'monthly_income', 'levy_rate', 'january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december', 'state_relief_fund', 'one_day_income', 'aid_fago', 'comment'] as $field)
                     <tr>
                         <th class="py-2 px-4">{{ ucwords(str_replace('_', ' ', $field)) }}</th>
                         <td class="py-2 px-4">{{ $membership->$field }}</td>
@@ -21,7 +16,8 @@
                 @endforeach
             </table>
 
-            <a href="{{ route('memberships.index') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-6 inline-block">
+            <a href="{{ route('memberships.index') }}"
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-6 inline-block">
                 Back to List
             </a>
         </div>
